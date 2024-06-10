40419504c80f91ddd241b5327ec81a52b7e27986
AndersonXV <andersonxd729@gmail.com>
2024-06-10 15:40:23 -0400

    add1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './Components/Login'
-
+import ShowAllTask from './Components/ShowAllTask'
+import AddTask from './Components/AddTask'
+import UpdateTask from './Components/UpdateTask'
+import TaskNotFound from './Components/TaskNotFound'
 function App() {
 
   return (
     <>
     <Routes>
       <Route path ="/" element={<Login />} />
-      <Route path ="/view" element={<h1>view page</h1>} />
+      <Route path ="/add" element={<AddTask />} />
+      <Route path ="/view" element={<ShowAllTask />} />
+      <Route path ="/update/:id" element={<UpdateTask />} />
+      <Route path ="*" element={<TaskNotFound />} />
     </Routes>
     </>
   )
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleUser = (e) => {
+        sessionStorage.setItem("user", e.target.value)
+        //sessionStorage.getItem()
+        // sessionStorage.clear()      
+    }
+    const handleLogin = () => {
+        navigate("/add") 
+    }
+
     return (
         <div className='wrapper'>
             <form action=''>
@@ -13,9 +25,11 @@ const Login = () => {
                             <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
                         </svg>
                     </span>
-                    <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Username" />
+                    <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Username" required onChange = {handleUser} />
                 </div>
-                <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
+                <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"onClick = {handleLogin}>Login</button>
+                
+
             </form>
 
         </div>
