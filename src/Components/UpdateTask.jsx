db49790a0dccb5a30e89e50f4cd522284d73c4cc
AndersonXV <andersonxd729@gmail.com>
2024-06-18 16:26:53 -0400

    TDT

--- src/Components/UpdateTask.jsx
+++ src/Components/UpdateTask.jsx
@@ -81,10 +81,10 @@ const navigate = useNavigate();
         <>
             {!msg ? (
                 <div>
-                    <p className="=text-x1 uAdd">Update Task</p>
+                    <p className="=text-x1 UT">Update Task</p>
 
 
-                    <form className="max-w-md mx-auto">
+                    <form className="max-w-md mx-auto uAdd">
 
                         <div className="relative z-0 w-full mb-5 group">
                             <input value={formData.task} type="title" name="task" id="task" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required onChange={handleChange} />
